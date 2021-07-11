a9d6b08960dd7ebe638a19b8f6bc2beb92cc85b6
no-brand <do.dream.david@gmail.com>
2021-07-11 23:19:10 +0900

    Create spring legacy project

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/zsh
+
+# ├── init.sh
+# ├── pom.xml
+# └── src
+#     └── main
+#         ├── resources
+#         └── webapp
+#             ├── WEB-INF
+#             │   └── web.xml
+#             └── index.jsp
+
+mvn archetype:generate \
+        -DgroupId=com.nobrand.legacy \
+        -DartifactId=spring-legacy \
+        -DarchetypeArtifactId=maven-archetype-webapp
+
